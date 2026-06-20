bfd254dd3213aa6d06e0aef54c6f83ab5ad3c3b5
Moe Hein Latt <baloot.mhl@gmail.com>
2026-06-20 23:59:51 +0630

    sec: apply global BranchScope to StockAdjustment, CustomerPayment and CustomerCreditLedger models

--- app/Models/CustomerCreditLedger.php
+++ app/Models/CustomerCreditLedger.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\BranchScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -9,6 +10,11 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 class CustomerCreditLedger extends BaseModel
 {
     use HasFactory;
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new BranchScope);
+    }
 
     /**
      * The attributes that are mass assignable.
--- app/Models/StockAdjustment.php
+++ app/Models/StockAdjustment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\BranchScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\Activitylog\LogOptions;
@@ -9,6 +10,11 @@ use Spatie\Activitylog\LogOptions;
 class StockAdjustment extends BaseModel
 {
     use HasFactory;
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new BranchScope);
+    }
 
     /**
      * The attributes that are mass assignable.
