8ac363b9b9d356fdda70ff36a6252081eec677b1
Moe Hein Latt <baloot.mhl@gmail.com>
2026-07-18 22:24:30 +0630

    fix(ui): delay layout toast mounting to prevent race conditions during full page SPA mounts

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -13,10 +13,13 @@ export default function AppLayout({ children, breadcrumbs, ...props }: AppLayout
     const { flash } = usePage<SharedData>().props;
 
     useEffect(() => {
-        if (flash.success) toast.success(flash.success);
-        if (flash.error) toast.error(flash.error);
-        if (flash.info) toast.info(flash.info);
-        if (flash.warning) toast.warning(flash.warning);
+        const timer = setTimeout(() => {
+            if (flash.success) toast.success(flash.success);
+            if (flash.error) toast.error(flash.error);
+            if (flash.info) toast.info(flash.info);
+            if (flash.warning) toast.warning(flash.warning);
+        }, 100);
+        return () => clearTimeout(timer);
     }, [flash]);
 
     return (
